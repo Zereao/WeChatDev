2daf2308376a029398de4e0b0d894390db41125e
Zereao <1050214520@qq.com>
2018-12-18 10:47:27 +0800

    H

---
+++ wechat.sh
@@ -0,0 +1,13 @@
+echo "脚本开始执行....."
+echo "[ git add . ] 准备执行....."
+git add .
+echo "[ git add . ] 执行完毕....."
+echo "[ git commit -m\"H\" ] 准备执行....."
+git commit -m"H"
+echo "[ git commit -m\"H\" ] 执行完毕....."
+echo "[ git pull ] 准备执行....."
+git pull
+echo "[ git pull ] 执行完毕....."
+echo "[ git push ] 准备执行....."
+git push
+echo "[ git push ] 执行完毕....."
