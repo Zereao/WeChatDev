bc9270221da85c3ccf7e540691bbd68bb5a0a204
Zereao <1050214520@qq.com>
2018-12-19 19:23:12 +0800

    H

--- src/main/java/com/zereao/wechat/dao/ResponseMsgDAO.java
+++ src/main/java/com/zereao/wechat/dao/ResponseMsgDAO.java
@@ -2,11 +2,13 @@ package com.zereao.wechat.dao;
 
 import com.zereao.wechat.data.bo.ResponseMsg;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Zereao
  * @version 2018/12/18  10:52
  */
+@Repository
 public interface ResponseMsgDAO extends MongoRepository<ResponseMsg, Integer> {
 
 }
